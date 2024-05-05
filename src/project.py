ff84f40360f4d2f8cb6048ea7457e704d3b55608
Dillon Weaver <weaver.dillon27@gmail.com>
2024-05-05 15:56:30 -0500

    Added emoji image revisions along with reorganized print layout

--- src/project.py
+++ src/project.py
@@ -1,10 +1,67 @@
-import turtle
 import random
 from time import sleep
+from PIL import Image
 
-def randomart(number):
-   idx = random.randrange(len(number))
-   return number[idx]
+def monalisa():
+   filename = "emojilisa.png"
+   img = Image.open(filename)
+   img.show()
+   printspeed("The Mona Lisa, the world's most famous painting!")
+   printspeed("10 Million people a year travel to see this iconic artwork.")
+   printspeed("The lady in the painting was a real woman named Lisa del Giocondo.")
+   printspeed("Although Leonardo da Vinci is most known for the Mona Lisa, it one of a very few amount of paintings he ever made.")
+   printspeed("👩‍🦱")
+   img.close()
+
+def starrynight():
+   filename = "emojinight.png"
+   img = Image.open(filename)
+   img.show()
+   printspeed("Van Gogh's classic work, The Starry Night!")
+   printspeed("The famous artist painted this masterpiece from a room in the Saint-Paul Asylum in 1889.")
+   printspeed("The giant black triangle in the center of the painting is actually a stylized cyprus tree.")
+   printspeed("Van Gogh could barely afford to paint at the time, but now The Starry Night is valued at almost 100 million dollars.")
+   printspeed("🌠🌙")
+   img.close()
+
+def pearlearring():
+   filename = "emojiearring.png"
+   img = Image.open(filename)
+   img.show()
+   printspeed("The Girl with a Pearl Earring, a classic dutch work by painter Johannes Vermeer.")
+   printspeed("Before becomming a classic, this painting was once lost for over 200 years!")
+   printspeed("The painting is a style of dutch painting called a Tronie, where human facial expressions are the primary focus.")
+   printspeed("Unlike the Mona Lisa, nobody knows who the girl in the painting is.")
+   printspeed("👩‍🦳◽")
+   img.close()
+
+def scream():
+   filename = "emojiscream.png"
+   img = Image.open(filename)
+   img.show()
+   printspeed("Edvard's Munch's iconic yet unsettling artwork, The Scream!")
+   printspeed("This painting is inspired by actual anxiety and panic attacks Munch had at the time.")
+   printspeed("Munch made multiple versions of The Scream that are on display in multiple museums.")
+   printspeed("The original Scream painting has been stolen twice! Once in 1994 and again in 2004.")
+   printspeed("😱")
+   img.close()
+
+def gothic():
+   filename = "emojigothic.png"
+   img = Image.open(filename)
+   img.show()
+   printspeed("American Gothic, the defining work of Grant Wood.")
+   printspeed("Wood was inspired to create this painting after seeing a house in Iowa that looked almost like a church.")
+   printspeed("Relative of Wood say he intended the people in the painting not to be husband and wife, but a father and daughter.")
+   printspeed("This painting is known for being a commentary on the preservation of the old culture of the midwest clashing with the new image of Modern America.")
+   printspeed("🧓👴")
+   img.close()
+
+
+def randomart(painting):
+   painting = [monalisa, starrynight, pearlearring, scream, gothic]
+   idx = random.randrange(len(painting))
+   return painting[idx]
 
 def printspeed(text):
    for e in text:
@@ -13,46 +70,30 @@ def printspeed(text):
    print()
    
 def main():
-    printspeed("Welcome to the Digital Art Museum presneted by Turtle!")
-    printspeed("Here you will see beautiful recreations of the world's most iconic paintings.")
+    printspeed("Welcome to the 🖌🖼🖋 EMOJI ART MUSEUM! 🖌🖼🖋")
+    printspeed("Here you will see some of the world's most iconic paintings recreated entirely through emojis! 😀")
     while True:
        art = int(input("Pick which painting you wish to see (1, 2 ,3 ,4, 5, 6 for a random painting or 7 to quit!): "))
-       numbers = (1, 2, 3, 4, 5)
 
        if art == 1:
-          printspeed("The Mona Lisa, the world's most famous painting!")
-          printspeed("10 Million people a year travel to see this iconic artwork.")
-          printspeed("The lady in the painting was a real woman named Lisa del Giocondo.")
-          printspeed("Although Leonardo da Vinci is most known for the Mona Lisa, it one of a very few amount of paintings he ever made.")
+          monalisa()
        elif art == 2:
-          printspeed("Van Gogh's classic work, The Starry Night!")
-          printspeed("The famous artist painted this masterpiece from a room in the Saint-Paul Asylum in 1889.")
-          printspeed("The giant black triangle in the center of the painting is actually a stylized cyprus tree.")
-          printspeed("Van Gogh could barely afford to paint at the time, but now The Starry Night is valued at almost 100 million dollars.")
+          starrynight()
        elif art == 3: 
-          printspeed("The Girl with a Pearl Earring, a classic dutch work by painter Johannes Vermeer.")
-          printspeed("Before becomming a classic, this painting was once lost for over 200 years!")
-          printspeed("The painting is a style of dutch painting called a Tronie, where human facial expressions are the primary focus.")
-          printspeed("Unlike the Mona Lisa, nobody knows who the girl in the painting is.")
+          pearlearring()
        elif art == 4:
-          printspeed("Edvard's Munch's iconic yet unsettling artwork, The Scream!")
-          printspeed("This painting is inspired by actual anxiety and panic attacks Munch had at the time.")
-          printspeed("Munch made multiple versions of The Scream that are on display in multiple museums.")
-          printspeed("The original Scream painting has been stolen twice! Once in 1994 and again in 2004.")
+          scream()
        elif art == 5:
-          printspeed("American Gothic, the defining work of Grant Wood.")
-          printspeed("Wood was inspired to create this painting after seeing a house in Iowa that looked almost like a church.")
-          printspeed("Relative of Wood say he intended the people in the painting not to be husband and wife, but a father and daughter.")
-          printspeed("This painting is known for being a commentary on the preservation of the old culture of the midwest clashing with the new image of Modern America.")
+          gothic()
        elif art == 6:
-          printspeed("Random selected, generating random painting.....")
-          randomart(numbers)
+          printspeed("Random selected, generating random painting 😎 .....")
+          randomart()
        elif art == 7:
           printspeed("Thanks for visiting the Digital Art Museum,")
-          printspeed("Hope to see you again soon!")
+          printspeed("Hope to see you again soon! 😄✋")
           break
        else:
-          printspeed("Whoops! Invalid input, choose again!")
+          printspeed("Whoops! Invalid input 😯 choose again!")
 
 
 if __name__ == "__main__":
